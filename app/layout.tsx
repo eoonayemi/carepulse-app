69fafc54e0b53fd231939e52615cae6252dc4fbd
Emmanuel <eoonayemi@gmail.com>
2025-10-08 18:37:54 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,50 @@
+import * as Sentry from "@sentry/nextjs";
+import type { Metadata } from "next";
+import "./globals.css";
+import { Plus_Jakarta_Sans as FontSans } from "next/font/google";
+import { ThemeProvider } from "next-themes";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-sans",
+});
+
+export function generateMetadata(): Metadata {
+  return {
+    // Your static metadata
+    title: "CarePulse",
+    description:
+      "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+    icons: {
+      icon: "/assets/icons/logo-icon.svg",
+    },
+
+    // Your dynamic metadata from Sentry
+    other: {
+      ...Sentry.getTraceData(),
+    },
+  };
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
